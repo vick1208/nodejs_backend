f9cce4731d16cf740c9d6f25148fb1f22fee6c32
Vicky AS <vickyhuang808@gmail.com>
2024-08-01 15:09:21 +0700

    membuat route user

--- tugas14-jwt-intro/src/routes.ts
+++ tugas14-jwt-intro/src/routes.ts
@@ -4,6 +4,8 @@ import uploadMiddleware from "./middlewares/upload.middleware";
 import uploadController from "./controllers/upload.controller";
 import productsController from "./controllers/products.controller";
 import categoriesController from "./controllers/categories.controller";
+import authController from "./controllers/auth.controller";
+import authMiddleware from "./middlewares/auth.middleware";
 
 const router = express.Router();
 
@@ -18,6 +20,11 @@ router.post("/categories",categoriesController.create);
 router.get('/categories/:id',categoriesController.findOne);
 router.put('/categories/:id',categoriesController.update);
 router.delete('/categories/:id',categoriesController.delete);
+
+router.post('/auth/login',authController.login);
+router.post('/auth/register',authController.register);
+router.get('/auth/me',authMiddleware,authController.me);
+router.post('/auth/profile',authMiddleware,authController.profile);
 
 router.post("/upload", uploadMiddleware.single, uploadController.single);
 router.post("/uploads", uploadMiddleware.multiple, uploadController.multiple);
